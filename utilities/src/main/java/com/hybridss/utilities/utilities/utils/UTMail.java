1126394b1acb4865a970972e62de85a8dc88c4fb
Jose Miguel Mercado Trujillo <jmercadot@elektra.com.mx>
2021-08-30 19:45:09 -0500

    mejoras en control de usuarios

--- utilities/src/main/java/com/hybridss/utilities/utilities/utils/UTMail.java
+++ utilities/src/main/java/com/hybridss/utilities/utilities/utils/UTMail.java
@@ -26,8 +26,8 @@ import eu.ocathain.javax.activation.FileDataSource;
 
 public class UTMail {
 
-    private final String username = "kops90@live.com";
-    private final String password = "migueloo2550082";
+    private String username = "kops90@live.com";
+    private String password = "migueloo2550082";
     private final ArrayList<String>  correos;
 
 
@@ -36,21 +36,14 @@ public class UTMail {
 
     }
 
+    public UTMail(ArrayList<String> correos, String username, String password) {
+        this.correos = correos;
+        this.username = username;
+        this.password = password;
+    }
+
     public boolean enviarBitacora(String subject) {
         boolean response = false;
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "outlook.office365.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
         try {
             String path = LGFileLogger.getmLogsPath();
 
@@ -63,14 +56,18 @@ public class UTMail {
                 addAttachment(multipart,log.getAbsolutePath());
             }
 
-            Message message = new MimeMessage(session);
+            Message message = new MimeMessage(getSesion());
             message.setFrom(new InternetAddress(username));
 
-            for (String correo : correos){
-                message.setRecipients(Message.RecipientType.TO,
-                        InternetAddress.parse(correo));
+            String[] recipientList = correos.toArray(new String[0]);
+            InternetAddress[] recipientAddress = new InternetAddress[recipientList.length];
+            int counter = 0;
+            for (String recipient : recipientList) {
+                recipientAddress[counter] = new InternetAddress(recipient.trim());
+                counter++;
             }
-            
+            message.setRecipients(Message.RecipientType.TO, recipientAddress);
+
             message.setSubject(subject);
             message.setContent(multipart);
 
@@ -87,6 +84,36 @@ public class UTMail {
         return response;
     }
 
+    private boolean enviarHTML(String html, String subject){
+        boolean response = false;
+
+        try {
+
+            Message message = new MimeMessage(getSesion());
+            message.setFrom(new InternetAddress(username));
+
+            for (String correo : correos){
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse(correo));
+            }
+
+            message.setSubject(subject);
+            message.setContent(html, "text/html; charset=utf-8");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+            response = true;
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (javax.mail.MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
     private void addAttachment(Multipart multipart, String filename) {
         try {
             DataSource source = new FileDataSource(filename);
@@ -100,4 +127,22 @@ public class UTMail {
 
     }
 
+    private Session getSesion(){
+        Session session = Session.getInstance(getProperties(),
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        return session;
+    }
+
+    private Properties getProperties(){
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "outlook.office365.com");
+        props.put("mail.smtp.port", "587");
+        return props;
+    }
 }
